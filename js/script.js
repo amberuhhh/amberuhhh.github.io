ac242da2e8721749b4f46efefc2e2184c2b54fd7
amberuhhh <amberkopas12@gmail.com>
2024-04-19 12:11:20 -0400

    !

--- js/script.js
+++ js/script.js
@@ -17,17 +17,3 @@ function closeMenu() {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }
-
-
-// //Hover over card, get effect
-// let card = document.querySelector('.item-card');
-      
-// function changeB() {
-//   card.style.border = 'solid black(1em)';
-// }
-// function changeBBack() {
-//   card.style.border = '';
-// }
-
-// card.addEventListener('mouseover', changeB);
-// card.addEventListener('mouseout', changeBBack);
